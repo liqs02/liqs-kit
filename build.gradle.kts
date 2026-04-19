5f96a2827b67f285a5aa1fc058fca6376593ac26
liqs02 <sprawnosc-zoologia-0n@icloud.com>
2026-04-19 09:46:21 +0200

    bootstrap project with Gradle + Apache 2.0 license

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    kotlin("plugin.jpa") version "2.2.21"
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.patryklikus"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+extra["springBootVersion"] = "4.0.3"
+extra["springModulithVersion"] = "2.0.3"
+
+dependencies {
+    compileOnly(platform("org.springframework.boot:spring-boot-dependencies:${property("springBootVersion")}"))
+    compileOnly(platform("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}"))
+
+    compileOnly("org.springframework:spring-context")
+    compileOnly("org.springframework:spring-webmvc")
+    compileOnly("jakarta.servlet:jakarta.servlet-api")
+    compileOnly("org.springframework:spring-orm")
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+    compileOnly("org.springframework.boot:spring-boot-hibernate")
+    compileOnly("org.springframework.data:spring-data-commons")
+    compileOnly("org.springframework.data:spring-data-jpa")
+    compileOnly("org.springframework.modulith:spring-modulith-core")
+    compileOnly("org.hibernate.orm:hibernate-core")
+    compileOnly("jakarta.persistence:jakarta.persistence-api")
+    compileOnly("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation(platform("org.springframework.boot:spring-boot-dependencies:${property("springBootVersion")}"))
+    testImplementation(platform("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-webtestclient")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-core")
+    testImplementation("org.springframework.modulith:spring-modulith-core")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.testcontainers:testcontainers-postgresql")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testRuntimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+    jvmToolchain(21)
+}
+
+java {
+    withSourcesJar()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
